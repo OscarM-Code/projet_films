34f3781630311167fffd37421d42b5ea0084db1e
Oscar Millot <oscarmillot23@gmail.com>
2021-07-08 23:15:33 +0200

    ajout des images et petite maj nom dossier

--- vue/index_vue.php
+++ vue/index_vue.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="./ressource/style.css">
+    <link rel="stylesheet" href="./assets/style.css">
     <title>projet_films</title>
 </head>
 <body>
@@ -86,6 +86,6 @@
     <p>CECI EST UN FOOTER</p>
     </footer>
 
-    <script src="ressource/script.js"></script>
+    <script src="assets/script.js"></script>
 </body>
 </html>
