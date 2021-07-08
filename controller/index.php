c81a1dcdf0a13866db56093a784fa25bac4a796d
happyvolt92 <elodiegage@gmail.com>
2021-07-08 11:08:45 +0200

    IDK

--- controller/index.php
+++ controller/index.php
@@ -1,22 +1,11 @@
 <?php
 
-$url = '';
-if(isset($_SERVER['REQUEST_URI'])){
-$url = explode('/', $_SERVER['REQUEST_URI']);
-}
-	 
-switch ($url) {
-    case $url[2] == '':
-        require './controller/index.php';
-        break;
-    case $url[2] == 'about_us':
-        require './controller/about_us.php';
-        break;
-    case $url[2] == 'film':
-        require './controller/film.php';
-        break;
-    default:
-        http_response_code(404);
-        require './controller/error.php';
-        break;
-    }
+require './model/model_index.php';
+
+$req = getMovies(5);
+
+$donnees = $req->fetch();
+
+require './vue/index_vue.php';
+
+?>
