98ea6b9d9b7c990d09d9af9185864c87024be189
Oscar Millot <oscarmillot23@gmail.com>
2021-07-10 15:53:43 +0200

    ajout d'apparence css

--- vue/film_vue.php
+++ vue/film_vue.php
@@ -15,12 +15,14 @@
 
 <div class="listDiv" href="#<?php echo $fullList["id_film"]; ?>">
     <img src="<?php echo $fullList["images"]; ?>" alt="affiche du films">
+
+    <button> <p><span>+</span> En savoir plus</p> </button>
+    
     <div>
-        <p> <?php echo $fullList["titre"]; ?> </p>
-        <p> <?php echo $fullList["sortie"]; ?> </p>
-        <p> <?php echo $fullList["realisateur"]; ?> </p>
-        <p> <?php echo $fullList["genre"]; ?> </p>
-        <p> <?php echo $fullList["synopsis"]; ?> </p>
+        <p> <span>Titre: </span> <span class="spanSearch"><?php echo $fullList["titre"]; ?></span> </p>
+        <p> <span>Année de sortie: </span> <span class="spanSearch"><?php echo $fullList["sortie"]; ?></span> </p>
+        <p> <span>Réalisateur(s): </span> <span class="spanSearch"><?php echo $fullList["realisateur"]; ?></span> </p>
+        <p> <span>Genre(s): </span> <span class="spanSearch"><?php echo $fullList["genre"]; ?></span> </p>
     </div>
 </div>
 
