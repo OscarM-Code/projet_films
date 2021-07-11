0fbd4d589f373222c31eadd9c65212d044dff7b4
Oscar Millot <oscarmillot23@gmail.com>
2021-07-11 20:26:26 +0200

    ajout page crédits + erreur 404 css etc tralala

--- vue/about_us_vue.php
+++ vue/about_us_vue.php
@@ -8,4 +8,48 @@
 
     </header>
 
-<h2>COUCOU</h2>
+<section id="about">
+    <h2>CRÉDITS</h2>
+
+    <div id="grid">
+
+        <div>
+            <img src="assets/images/user.png" alt="photo">
+            <h3>Élodie Gage</h3>
+            <p><span>Alias:</span></p>
+            <h3>Lorem Ipsum</h3>
+            <p><span>Rôle dans le projet:</span></p>
+            <h3>Dev back-end</h3>
+            <div class="link">
+                <a href="https://github.com/happyvolt92" target="blank"><img src="assets/images/github_logo.png" alt=""></a>
+                <a href="https://www.linkedin.com/in/%C3%A9lodie-gage-0b623b204/" target="blank"><img src="assets/images/linkedin_logo.png" alt=""></a>
+            </div>
+        </div>
+        <div>
+            <img src="assets/images/user.png" alt="photo">
+            <h3>Dylan CHAPUIS</h3>
+            <p><span>Alias:</span></p>
+            <h3>Lorem Ipsum</h3>
+            <p><span>Rôle dans le projet:</span></p>
+            <h3>Dev front-end</h3>
+            <div class="link">
+                <a href="https://github.com/CDylan-source" target="blank"><img src="assets/images/github_logo.png" alt=""></a>
+                <a href="https://www.linkedin.com/in/chapuis-dylan-373679210/" target="blank"><img src="assets/images/linkedin_logo.png" alt=""></a>
+            </div>
+        </div>
+        <div>
+            <img src="assets/images/user.png" alt="photo">
+            <h3>Oscar Millot</h3>
+            <p><span>Alias:</span></p>
+            <h3>Lorem Ipsum</h3>
+            <p><span>Rôle dans le projet:</span></p>
+            <h3>Chef de projet</h3>
+            <div class="link">
+                <a href="https://github.com/OscarM-Code" target="blank"><img src="assets/images/github_logo.png" alt=""></a>
+                <a href="https://www.linkedin.com/in/oscar-millot/" target="blank"><img src="assets/images/linkedin_logo.png" alt=""></a>
+            </div>
+        </div>
+
+    </div>
+
+</section>
--- vue/error_vue.php
+++ vue/error_vue.php
@@ -1,1 +1,21 @@
-<h1>ERROR 404 ! PAGE NOT FOUND </h1>
+<ul>
+
+            <li><a href="./">ACCEUIL</a></li>
+            <li><a id="there" href="./film">RECHERCHE</a></li>
+            <li><a href="./credits">CRÉDITS</a></li>
+
+        </ul>
+
+    </header>
+
+    <section id="errorB">
+
+        <div>
+            <h2>404 - CETTE PAGE N'EXISTE PAS - 404</h2>
+        </div>
+
+        <div>
+            <img src="./assets/images/coffin_error.gif" alt="image de gens qui dancent">
+        </div>
+
+    </section>
