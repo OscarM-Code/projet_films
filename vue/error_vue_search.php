2afde002a35df3f961d1223bcef7998370cce81a
Oscar Millot <oscarmillot23@gmail.com>
2021-07-11 17:23:58 +0200

    ajout css erreur de recherche

--- vue/error_vue_search.php
+++ vue/error_vue_search.php
@@ -1,1 +1,10 @@
-<h1>PAS DE RÉSULTATS POUR CETTE RECHERCHE</h1>
+<section id="error">
+    <div>
+        <h2>PAS DE RÉSULTATS POUR CETTE RECHERCHE</h2>
+    </div>
+
+    <div>
+        <img src="./assets/images/coffin_error.gif" alt="image de gens qui dancent">
+    </div>
+
+</section>
