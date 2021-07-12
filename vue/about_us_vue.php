53c4a287b3ef1487c612f3ea6d57d3f3b80c328a
happyvolt92 <elodiegage@gmail.com>
2021-07-12 10:23:07 +0200

    mods droit rewrite, ajout Htaccess, mods page crédits

--- vue/about_us_vue.php
+++ vue/about_us_vue.php
@@ -13,39 +13,39 @@
     <div id="grid">
 
         <div>
-            <img src="assets/images/user.png" alt="photo">
+            <img src="assets/images/user.png" alt="Élodie GAGE, dev back-end">
             <h3>Élodie Gage</h3>
             <p><span>Alias:</span></p>
             <h3>happyvolt</h3>
             <p><span>Rôle dans le projet:</span></p>
             <h3>Dev back-end</h3>
             <div class="link">
-                <a href="https://github.com/happyvolt92" target="blank"><img src="assets/images/github_logo.png" alt=""></a>
+                <a href="https://github.com/happyvolt92" target="blank"><img src="assets/images/github_logo.png" alt="dev back-end"></a>
                 <a href="https://www.linkedin.com/in/%C3%A9lodie-gage-0b623b204/" target="blank"><img src="assets/images/linkedin_logo.png" alt=""></a>
             </div>
         </div>
         <div>
-            <img src="assets/images/user.png" alt="photo">
+            <img src="assets/images/user.png" alt="Dylan CHAPUIS">
             <h3>Dylan CHAPUIS</h3>
             <p><span>Alias:</span></p>
             <h3>Lorem Ipsum</h3>
             <p><span>Rôle dans le projet:</span></p>
             <h3>Dev front-end</h3>
             <div class="link">
-                <a href="https://github.com/CDylan-source" target="blank"><img src="assets/images/github_logo.png" alt=""></a>
+                <a href="https://github.com/CDylan-source" target="blank"><img src="assets/images/github_logo.png" alt="dev Front-end github"></a>
                 <a href="https://www.linkedin.com/in/chapuis-dylan-373679210/" target="blank"><img src="assets/images/linkedin_logo.png" alt=""></a>
             </div>
         </div>
         <div>
-            <img src="assets/images/user.png" alt="photo">
+            <img src="assets/images/user.png" alt="Oscar millot, L'arme secrète">
             <h3>Oscar Millot</h3>
             <p><span>Alias:</span></p>
-            <h3>Lorem Ipsum</h3>
+            <h3>L'arme secrète</h3>
             <p><span>Rôle dans le projet:</span></p>
             <h3>Chef de projet</h3>
             <div class="link">
-                <a href="https://github.com/OscarM-Code" target="blank"><img src="assets/images/github_logo.png" alt=""></a>
-                <a href="https://www.linkedin.com/in/oscar-millot/" target="blank"><img src="assets/images/linkedin_logo.png" alt=""></a>
+                <a href="https://github.com/OscarM-Code" target="blank"><img src="assets/images/github_logo.png" alt="Chef de Projet github"></a>
+                <a href="https://www.linkedin.com/in/oscar-millot/" target="blank"><img src="assets/images/linkedin_logo.png" alt="Chef de Projet LinkedIn"></a>
             </div>
         </div>
 
