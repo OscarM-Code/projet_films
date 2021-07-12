513a142ecf13a9f956c6ef32fdb35e6dbd9f9aed
happyvolt92 <elodiegage@gmail.com>
2021-07-12 06:57:26 +0200

    accept mods Oscar

--- vue/about_us_vue.php
+++ vue/about_us_vue.php
@@ -1,26 +1,50 @@
-<<<<<<< HEAD
-<!DOCTYPE html>
-<html lang="fr">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    <h1>COUCOU</h1>
-</body>
-</html>
-=======
 <ul>
 
             <li><a href="./">ACCEUIL</a></li>
             <li><a href="./film">RECHERCHE</a></li>
             <li><a id="there" href="./credits">CRÉDITS</a></li>
-
         </ul>
 
     </header>
 
-<h2>COUCOU</h2>
->>>>>>> d1022667ac98f0765dd2ee9e3883fe54a108f357
+<section id="about">
+    <h2>CRÉDITS</h2>
+    <div id="grid">
+        <div>
+            <img src="assets/images/user.png" alt="photo">
+            <h3>Élodie Gage</h3>
+            <p><span>Alias:</span></p>
+            <h3>Lorem Ipsum</h3>
+            <p><span>Rôle dans le projet:</span></p>
+            <h3>Dev back-end</h3>
+            <div class="link">
+                <a href="https://github.com/happyvolt92" target="blank"><img src="assets/images/github_logo.png" alt=""></a>
+                <a href="https://www.linkedin.com/in/%C3%A9lodie-gage-0b623b204/" target="blank"><img src="assets/images/linkedin_logo.png" alt=""></a>
+            </div>
+        </div>
+        <div>
+            <img src="assets/images/user.png" alt="photo">
+            <h3>Dylan CHAPUIS</h3>
+            <p><span>Alias:</span></p>
+            <h3>Lorem Ipsum</h3>
+            <p><span>Rôle dans le projet:</span></p>
+            <h3>Dev front-end</h3>
+            <div class="link">
+                <a href="https://github.com/CDylan-source" target="blank"><img src="assets/images/github_logo.png" alt=""></a>
+                <a href="https://www.linkedin.com/in/chapuis-dylan-373679210/" target="blank"><img src="assets/images/linkedin_logo.png" alt=""></a>
+            </div>
+        </div>
+        <div>
+            <img src="assets/images/user.png" alt="photo">
+            <h3>Oscar Millot</h3>
+            <p><span>Alias:</span></p>
+            <h3>Lorem Ipsum</h3>
+            <p><span>Rôle dans le projet:</span></p>
+            <h3>Chef de projet</h3>
+            <div class="link">
+                <a href="https://github.com/OscarM-Code" target="blank"><img src="assets/images/github_logo.png" alt=""></a>
+                <a href="https://www.linkedin.com/in/oscar-millot/" target="blank"><img src="assets/images/linkedin_logo.png" alt=""></a>
+            </div>
+        </div>
+    </div>
+</section>
--- vue/error_vue.php
+++ vue/error_vue.php
@@ -1,17 +1,1 @@
-<<<<<<< HEAD
-<!DOCTYPE html>
-<html lang="fr">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>ERROR 404</title>
-</head>
-<body>
-    <h1>ERROR 404 ! PAGE NOT FOUND </h1>
-    <img src="/projet_films/vue/images/tools.jpg" alt="boîte à outil" width="150px" height="150px" >
-</body>
-</html>
-=======
 <h1>ERROR 404 ! PAGE NOT FOUND </h1>
->>>>>>> d1022667ac98f0765dd2ee9e3883fe54a108f357
